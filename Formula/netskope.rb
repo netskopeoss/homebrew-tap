939a5e205ef849130bca70c1bc49c2437e14d8ae
ns-bray <bray@netskope.com>
2026-04-24 09:53:50 -0400

    Update netskope to 1.3.1

--- Formula/netskope.rb
+++ Formula/netskope.rb
@@ -3,8 +3,8 @@ class Netskope < Formula
 
   desc "Official CLI for the Netskope Security Cloud platform"
   homepage "https://github.com/netskopeoss/netskope-cli"
-  url "https://files.pythonhosted.org/packages/a1/53/de4c13637e4b7ca6f302bc4cc80802bfc0663ecc08c2e46cc4e4e8bedd7e/netskope-1.3.0.tar.gz"
-  sha256 "57d18e25cf08904f264a4a4ff981756351bb85cd766bd12576fa0be3132ab10e"
+  url "https://files.pythonhosted.org/packages/55/18/c9a3423174b10ba6751099fae5581fc0981bc0c6e9c5953154bb1a294b6d/netskope-1.3.1.tar.gz"
+  sha256 "e5fc9753eb83e905f39d3b4c019ea04406fd3c39a25d8154423d557ea94db0ab"
   license "MIT"
 
   depends_on "python@3.13"
